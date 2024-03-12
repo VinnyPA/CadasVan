7e8edc973888d8fdb5bc486e95dda99e54a70d9d
vinny <vinny@DESKTOP-T9UH72F>
2024-03-11 21:31:04 -0300

    Adicionar arquivos de projeto.

---
+++ Cadasvan/Models/Motorista.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cadasvan.Models
+{
+    public class Motorista
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }    
+        public string Endereco { get; set; } 
+        
+    }
+}
